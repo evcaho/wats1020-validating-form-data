a0855e8f722cb3c84eefde0c4fd4287e94d7e38f
evcaldwell <evcaldwell@gmail.com>
2015-08-09 23:16:34 -0400

    first part of project done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 // For this assignment you'll need to do a few things:
 // 1. Create a document ready handler.
+$(document).on('ready', function(){
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+});
